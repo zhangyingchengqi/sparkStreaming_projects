0048478736c0b8ea11590b1aade0ba00585610c3
zhangyingchengqi <1069595532@qq.com>
2020-06-08 19:17:28 +0800

    dao层的 MysqlPool主要提供了数据库联接池

--- src/main/scala/project2_wordcounts/streamingWC/dao/MysqlPool.scala
+++ src/main/scala/project2_wordcounts/streamingWC/dao/MysqlPool.scala
@@ -11,12 +11,14 @@ import project2_wordcounts.streamingWC.utils.Conf
  * Mysql连接池类
  *
  */
-class MysqlPool extends Serializable {
+class MysqlPool extends Serializable {   //可序列化
+  // 瞬态 的属性
   @transient lazy val log = LogManager.getLogger(this.getClass)
 
   private val cpds: ComboPooledDataSource = new ComboPooledDataSource(true)
   //private val dds=new DruidDataSource(  )
-  private val conf = Conf.mysqlConfig
+
+  private val conf = Conf.mysqlConfig    // 参数配置
   try {
 //    dds.setUsername(conf.get("username").getOrElse("root"))
 //    dds.setPassword(conf.get("password").getOrElse("a"))
@@ -24,8 +26,6 @@ class MysqlPool extends Serializable {
 //    dds.setDriverClassName("com.mysql.jdbc.Driver")
 //    dds.setInitialSize(   3 )
 //    dds.setMaxActive(200)
-
-
     cpds.setJdbcUrl(conf.get("url").getOrElse("jdbc:mysql://localhost:3306/word_freq?useUnicode=true&amp;characterEncoding=UTF-8"));
     cpds.setDriverClass("com.mysql.jdbc.Driver");
     cpds.setUser(conf.get("username").getOrElse("root"));
@@ -44,6 +44,7 @@ class MysqlPool extends Serializable {
     case e: Exception =>
       log.error("[MysqlPoolError]", e)
   }
+
   def getConnection: Connection = {
     try {
       return cpds.getConnection();
@@ -54,9 +55,10 @@ class MysqlPool extends Serializable {
     }
   }
 }
-//单例模型
+//单例模型:  构建方法私有化，对外提供唯一创建的方法
 object MysqlManager {
   var mysqlManager: MysqlPool = _
+
   def getMysqlManager: MysqlPool = {
     synchronized {
       if (mysqlManager == null) {
