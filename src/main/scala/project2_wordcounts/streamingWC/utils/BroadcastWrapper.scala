54bb155e64f9631a9ef5d2189dfed977c07329cd
zhangyingchengqi <1069595532@qq.com>
2020-06-10 19:04:28 +0800

    广播变量的一个封装类，用于提供对广播变量的修改操作

--- src/main/scala/project2_wordcounts/streamingWC/utils/BroadcastWrapper.scala
+++ src/main/scala/project2_wordcounts/streamingWC/utils/BroadcastWrapper.scala
@@ -1,7 +1,7 @@
 package project2_wordcounts.streamingWC.utils
 
 
-import java.io.{ ObjectInputStream, ObjectOutputStream }
+import java.io.{ObjectInputStream, ObjectOutputStream}
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.streaming.StreamingContext
 import scala.reflect.ClassTag
@@ -17,20 +17,21 @@ import scala.reflect.ClassTag
 case class BroadcastWrapper[T: ClassTag](
                                           @transient private val ssc: StreamingContext,
                                           @transient private val _v: T) {
-
+  // 创建广播变量
   @transient private var v = ssc.sparkContext.broadcast(_v)
 
   /**
    * 广播变量是只读的，可以利用spark的unpersist()，它按照LRU( lease Recently used)最近最久没有使用原则删除老数据。
+   *
    * @param newValue
    * @param blocking
    */
   def update(newValue: T, blocking: Boolean = false): Unit = {
-    v.unpersist(blocking)
+    v.unpersist(blocking) //删除缓存
     v = ssc.sparkContext.broadcast(newValue)
   }
 
-  def value: T = v.value
+  def value: T = v.value //对外提供一个函数用于访问这个广播变量 , 体现了封装
 
   private def writeObject(out: ObjectOutputStream): Unit = {
     out.writeObject(v)
